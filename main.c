6e73d88910bd34324862e5fa81a1fe7202eec5a3
Troy Hamilton <110515824+hamiltontroy@users.noreply.github.com>
2022-12-24 20:06:13 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -4,29 +4,29 @@
 
 void task(void *arg)
 {
-	printf("%d\n", *(int*)arg);
+    printf("%d\n", *(int*)arg);
     sleep(1);
 }
- 
+
 int main()
 {
-	threadpool
+    threadpool
         thpool;
     int
         a = 1111;
-        
+
     // Making threadpool with 2 threads
     thpool = thpool_init(2);
-    
-	// Adding 20 tasks to threadpool
-	for (int i = 0; i < 20; i++)
+
+    // Adding 20 tasks to threadpool
+    for (int i = 0; i < 20; i++)
     {
-		thpool_add_work(thpool, task, &a);
-	}
+        thpool_add_work(thpool, task, &a);
+    }
 
     // (blocking) Waits for threads residing within pool to finish tasks
-	thpool_wait(thpool);
+    thpool_wait(thpool);
 
-	// Deallocates all threads to free memory
-	thpool_destroy(thpool);
+    // Deallocates all threads to free memory
+    thpool_destroy(thpool);
 }
